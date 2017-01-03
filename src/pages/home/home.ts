7fdaaef6074a77dbdd3a4917ba672ad6bcaa9fef
u4bi <u4bi@u4bi.com>
2017-01-04 01:49:49 +0900

    alert button createUser init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController} from 'ionic-angular';
 import { FirebaseListObservable, AngularFireDatabase} from 'angularfire2';
 
 @Component({
@@ -8,9 +8,48 @@ import { FirebaseListObservable, AngularFireDatabase} from 'angularfire2';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  user: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController,
+              public alertController: AlertController,
+              public database: AngularFireDatabase) {
+    this.user = this.database.list('/agenda');
   }
-
+  
+  createUser(){
+    let newUserModal = this.alertController.create({
+      title: 'user',
+      message: 'plz',
+      inputs: [
+        {
+          name : 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'age',
+          placeholder: 'Age'
+        }
+      ],
+      buttons:[
+        {
+          text: 'exit',
+          handler: data =>{
+            console.log('exit');
+          }
+        },
+        {
+          text: 'Add',
+          handler: data =>{
+            this.user.push({
+              u_name: data.name,
+              u_age: data.age
+            });
+          }
+        }
+      ]
+    });
+   
+    newUserModal.present(newUserModal);
+  }
 }
