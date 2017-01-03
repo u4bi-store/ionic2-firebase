88ef06e957945cb255e570ce4a97008a3d098bea
u4bi <u4bi@u4bi.com>
2017-01-04 01:20:20 +0900

    angularFireModule import firebase config init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,18 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import {AngularFireModule} from 'angularfire2';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDnXo3gy2cLF-WS7AByVaWhARgu_gPaj7A",
+  authDomain: "u4bi-crud-app.firebaseapp.com",
+  databaseURL: "https://u4bi-crud-app.firebaseio.com",
+  storageBucket: "u4bi-crud-app.appspot.com",
+  messagingSenderId: "328438803411"
+};
 
 @NgModule({
   declarations: [
@@ -9,7 +20,8 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { FirebaseListObservable, AngularFireDatabase} from 'angularfire2';
 
 @Component({
   selector: 'page-home',
